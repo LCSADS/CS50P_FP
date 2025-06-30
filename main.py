17130dd879bc71f4fb26abda341cfc60df00249c
LCSADS <lucas.a.desimone@gmail.com>
2025-06-30 15:06:19 -0300

    implement download function with custom local folder using os.makedirs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pytubefix import YouTube
+from time import sleep
 import sys
+import os
 def main():
     while True:
         youtube_url = get_video_url()
@@ -14,7 +16,7 @@ def main():
             print("Something went wrong")
             continue
 # check if its' the correct video for better user experience
-        confirmation = input(f"{title} is the video you want to download?\n Y/N")
+        confirmation = input(f"{title} is the video you want to download?\n Type [Y] or [N] ")
         while confirmation not in "YyNn":
             confirmation = input("Invalid choice. Type only 'Y' or 'N'")
         if confirmation in 'Yy':
@@ -26,6 +28,10 @@ def main():
             break
         else:
             print("Ok, let's start again.")
+    chosen_file = video_resolution_choice(yt_object,chosen_format)
+    download_file(chosen_file)
+    
+
 
 
 
@@ -56,12 +62,46 @@ def ask_format():
 
 
 # choose quality
-# the youtube object generates stream objects inside of it, listed in another object, streamquery
+# the youtube object generates stream objects inside of it, listed inside another object, streamquery
 # manipulating the stream obj inside streamquery obj enables me to use methods like .download and atributes like filesize
-def video_resolution_choice(yt,chosen_format):
+def video_resolution_choice(yt_object,chosen_format):
     if chosen_format == 'video':
-        # streamquery here
-        download_formats = yt.streams.filter(progressive=True,file_extension="mp4").order_by("resolution")
+        # streamquery here                    filter audio+video files                order by decreasing resolution                        
+        download_formats = yt_object.streams.filter(progressive=True,file_extension="mp4").order_by("resolution").desc()
+# in case there's only one download option avaible
+        if len(download_formats) == 1:
+            print(f"Only one option avaible: {download_formats[0].resolution}")
+            chosen_file = download_formats[0]
+            return chosen_file
+# in case there are multiple avaible options for the user to select. Iterates over the list, returns the video choice. While loop for catching errors and better ux.
+        else:
+            print("Avaible options for download: ")
+            for i, video in enumerate(download_formats, start=1):
+                print(f"[{i}]{video.resolution} - {round(video.filesize / (1024*1024), 2)} MB")
+
+            while True:
+                try:
+                    user_choice = int(input("Type the corresponding number to select a resolution."))
+                    if user_choice in range(1,len(download_formats)+1):
+                        chosen_file = download_formats[user_choice - 1]
+                        break
+                    else:
+                        print("Please type the corresponding number with the option you want to select.")
+                except ValueError:
+                    print("Please type a valid number")
+        return chosen_file
+    
+def download_file(chosen_file):
+    os.makedirs("downloads",exist_ok=True)
+    print("Please wait",end='',flush=True)
+    for _ in range(3):
+        sleep(0.5)
+        print(".",end='',flush=True)
+    chosen_file.download(output_path="downloads")
+    print("\nDownload completed.")
+
+        
+
     
 
 # transcribe audio
